9536cc5730b7a37790ee178a0e6e7fbdd76b724c
Spxg <itsme@spxg.me>
2020-07-09 14:30:58 +0800

    ready to publish

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! # BlockDevice trait
+//! ```rust
+//! pub trait BlockDevice {
+//!     type Error;
+//!     fn read(&self, buf: &mut [u8], address: u32, number_of_blocks: u32) -> Result<(), Self::Error>;
+//!     fn write(&self, buf: &[u8], address: u32, number_of_blocks: u32) -> Result<(), Self::Error>;
+//! }
+//! ```
+
 #![no_std]
 
 /// BlockDevice trait
